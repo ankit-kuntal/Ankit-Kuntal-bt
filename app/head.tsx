8f4e1b78b4cde2b5e2fc66cda69513a0b7f07bbb
ankit kuntal <ankitkuntal904@gmail.com>
2026-03-15 21:28:22 +0530

    final GSC problem

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="google11818d60c71f8f69" />
+      <title>Dev | Full-Stack Developer Portfolio</title>
+      <meta name="description" content="Discover Dev’s software products, projects, blog and contact info." />
+    </>
+  )
+}
